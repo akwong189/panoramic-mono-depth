fa18d2d746ebd8e14478e72fff4fa1366b592aff
Alterac8 <awong189@calpoly.edu>
2022-05-05 22:07:30 -0700

    kitti loader

---
+++ kitti_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pathlib import Path
+
+
+# /data3/awong/kitti/train/2011_09_26_drive_0001_sync/proj_depth/groundtruth/image_02
+# /data3/awong/kitti_raw/2011_09_26/2011_09_26_drive_0001_sync/image_02/data
+
+
+def convert_depth_image_path(path):
+	p = Path(path).parts
+	
+	pd.read_csv()
+
+def create_train(path: str) :
+	pass
